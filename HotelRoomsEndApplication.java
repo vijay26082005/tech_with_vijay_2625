aa3dff40cded71c03df2e05ee21097be67cbbec2
VIJAY KRISHNA S <vijaykrishnas2005@gmail.com>
2025-07-04 22:00:07 +0530

    HotelRoomsEndApplication.java

---
+++ HotelRoomsEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.HotelRoomsEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelRoomsEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelRoomsEndApplication.class, args);
+	}
+
+}
